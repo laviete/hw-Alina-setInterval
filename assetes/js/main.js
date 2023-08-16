f78c9e0eec27802604db7d3e3cc7d6b1d79b0e7e
alina <a03072006@gmail.com>
2023-08-17 00:35:11 +0300

    completion task

---
+++ assetes/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+// забрати елемент з id="text" в змінну
+// зробити зворотній виклик від 10 до 1 - числа повинні
+// змінюватися через секунду setInterval(функція яка змінює
+// вміст елемента і зменшує число, 1000)
+// коли дійшли до 0 - зупинитися - перевіряємо коли робити clearInterval
+
+const h1 = document.getElementById("text");
+
+function counter() {
+    let count = h1.innerText;
+    //let count = 10;
+    const interval = setInterval(() => {
+      count--;
+      h1.innerText = count;
+      if (count === 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+  counter();
+
+
